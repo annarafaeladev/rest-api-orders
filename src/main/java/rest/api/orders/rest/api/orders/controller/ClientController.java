a4a362839dd22ee9e8bbf3b95b33c7a4b08b4e76
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-20 10:01:39 -0300

    create controller Client

--- src/main/java/rest/api/orders/rest/api/orders/controller/ClientController.java
+++ src/main/java/rest/api/orders/rest/api/orders/controller/ClientController.java
@@ -1,9 +1,37 @@
 package rest.api.orders.rest.api.orders.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import rest.api.orders.rest.api.orders.domain.Client;
+import rest.api.orders.rest.api.orders.dto.ClientCreateDto;
+import rest.api.orders.rest.api.orders.repositories.ClientRepository;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Object> create(@RequestBody @Valid ClientCreateDto clientCreateDto, UriComponentsBuilder uriComponentsBuilder){
+        var client = new Client(clientCreateDto);
+        var uri  = uriComponentsBuilder.path("/clients/{id}").buildAndExpand(client.getId()).toUri();
+
+        clientRepository.save(client);
+
+        return ResponseEntity.created(uri).body(client);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> index(){
+        var products = clientRepository.findAll();
+
+        return ResponseEntity.ok(products.stream().toList());
+    }
 }
--- src/main/java/rest/api/orders/rest/api/orders/domain/Client.java
+++ src/main/java/rest/api/orders/rest/api/orders/domain/Client.java
@@ -1,8 +1,12 @@
 package rest.api.orders.rest.api.orders.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import rest.api.orders.rest.api.orders.dto.ClientCreateDto;
+import rest.api.orders.rest.api.orders.dto.ProductCreateDto;
+
+import java.util.UUID;
 
 @Entity(name = "Client")
 @Table(name = "clients")
@@ -12,6 +16,26 @@ import lombok.*;
 @Setter
 @EqualsAndHashCode
 public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false, unique = true, length = 14)
+
+    private String cpf;
+
+    @NotBlank
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    public Client(ClientCreateDto clientCreateDto){
+        this.email = clientCreateDto.email();
+        this.cpf = clientCreateDto.cpf();
+        this.name = clientCreateDto.name();
+    }
 
 }
